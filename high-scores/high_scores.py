5e4abbb9e0c32072b6168f8df2dbf30b328cbb60
Jess <jlpalardy@gmail.com>
2020-02-10 16:38:43 +0100

    First commit

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -2,13 +2,16 @@ def latest(scores):
 	return scores[len(scores)-1]
 
 def personal_best(scores):
-	scores.sort()
-	return scores[len(scores)-1]
+	scoreSorted = scores.copy()
+	scoreSorted.sort()
+	return scoreSorted[len(scoreSorted)-1]
 
 def personal_top_three(scores):
-	scores.sort(reverse = True)
-	if len(scores) >= 3:
-		return scores[0:3]
+	scoreSorted = scores.copy()
+	scoreSorted.sort(reverse = True)
+	if len(scoreSorted) >= 3:
+		return scoreSorted[0:3]
 	else:
-		return scores
-	
+		return scoreSorted
+
+
