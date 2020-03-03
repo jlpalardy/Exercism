4992b760efbc22c73bdc432c6d3f95d18ce6a28e
Jess <jlpalardy@gmail.com>
2020-03-03 16:38:58 +0100

    Made matrix.py more pythonic with some list comprehensions

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,12 +1,6 @@
 class Matrix:
 	def __init__(self, matrix_string):
-		self.matrix2D = []
-		rowsArray = matrix_string.split('\n')
-		for row in rowsArray:
-			self.matrix2D.append(row.split())
-		#Notice matrix2D is a 2D array- a list of lists, where each element
-		#is a row of the matrix
-		self.numRows = len(rowsArray)
+		self.matrix2D = [row.split() for row in matrix_string.split('\n')]
 
 		#The following checks to make sure that the rows are all the same
 		#length- if they're not, that indicates that there are some column
@@ -20,17 +14,12 @@ class Matrix:
 			raise ValueError('All rows must be the same length!')
 		else:
 			self.numCols = rowLengths.pop()
-		
-		#Up until now, all the elements of our matrix are strings! Convert
-		#them to integers.
-		for i in range(self.numRows):
-			for j in range(self.numCols):
-				self.matrix2D[i][j] = int(self.matrix2D[i][j])		
 
 	def row(self, index):
-		return self.matrix2D[index - 1]
+		return [int(x) for x in self.matrix2D[index - 1]]
 
 	def column(self, index):
-		return [row[index - 1] for row in self.matrix2D]
+		column = [row[index - 1] for row in self.matrix2D]
+		return [int(x) for x in column]
 
 	
